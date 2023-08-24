7beaffeb30c3edfbd9f23e31e86a0aeb02ed48bb
김영우 <ywkim0114@riskzero.kr>
2023-08-24 17:06:53 +0900

    데이터 테이블 만들어 보기

--- src/components/dataTable/index.tsx
+++ src/components/dataTable/index.tsx
@@ -1,55 +1,39 @@
-import { Colum, Row } from "../../pages/datatable";
-import styled from "styled-components";
+import React from "react";
+import { Column } from "../../pages/datatable";
 
-interface DateTableProps {
-  columns: Colum[];
-  rows: Row[];
+interface DataType {
+    Colums: Column[];
+    Rows: any[];
 }
 
-export default function DataTable({ columns, rows }: DateTableProps) {
-  return (
-    <StyledDataTable>
-      <thead>
-        <tr>
-          {columns.map((obj, i) => (
-            <th key={i}>{obj.label}</th>
-          ))}
-        </tr>
-      </thead>
-
-      <tbody>
-        {rows.map((row, i) => {
-          return (
-            <tr key={i}>
-              {columns.map((col, j) => {
-                const value = col.format ? col.format(col, row) : row[col.id];
-                return (
-                  <td
-                    key={j}
-                    style={{ textAlign: col.align || ("center" as any) }}
-                  >
-                    {value}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </StyledDataTable>
-  );
+export default function DataTable({ Colums, Rows }: DataType) {
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        {Colums.map((col, index) => {
+                            return <th key={index}>{col.label}</th>;
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {Rows.map((row, index: number) => {
+                        return (
+                            <tr key={index}>
+                                <td>{row.name}</td>
+                                <td>{row.old}</td>
+                                <td>{row.skill}</td>
+                                <td>
+                                    {Colums.map((col: any) => {
+                                        return col.format ? col.format("", row) : "";
+                                    })}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
+    );
 }
-
-const StyledDataTable = styled.table`
-  width: 500px;
-  border-collapse: collapse;
-
-  tr,
-  th {
-    border: 1px #ccc solid;
-  }
-
-  th {
-    background-color: #e2e6e8;
-  }
-`;
